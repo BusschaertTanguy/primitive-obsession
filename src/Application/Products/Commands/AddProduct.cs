f3dcf967051dfd0b87ea7b054665c05891bc9655
Tanguy Busschaert <tanguy.busschaert.95@gmail.com>
2022-11-07 21:33:51 +0100

    add remaining handlers in v1

---
+++ src/Application/Products/Commands/AddProduct.cs
@@ -0,0 +1,29 @@
+﻿using Application.Common.Commands;
+using Domain.Products.Entities;
+using Domain.Products.Repositories;
+
+namespace Application.Products.Commands;
+
+public static class AddProduct
+{
+    public sealed record Command(Guid Id, string Name, decimal Price);
+
+    internal sealed class Handler : ICommandHandler<Command>
+    {
+        private readonly IProductRepository _productRepository;
+
+        public Handler(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        public Task Handle(Command command)
+        {
+            var (id, name, price) = command;
+
+            var product = new Product(id, name, price);
+
+            return _productRepository.Save(product);
+        }
+    }
+}
---
+++ src/Application/Products/Commands/ChangeProductName.cs
@@ -0,0 +1,30 @@
+﻿using Application.Common.Commands;
+using Domain.Products.Repositories;
+
+namespace Application.Products.Commands;
+
+public static class ChangeProductName
+{
+    public sealed record Command(Guid ProductId, string Name);
+
+    internal sealed class Handler : ICommandHandler<Command>
+    {
+        private readonly IProductRepository _productRepository;
+
+        public Handler(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        public async Task Handle(Command command)
+        {
+            var (productId, name) = command;
+
+            var product = await _productRepository.GetById(productId);
+
+            product.ChangeName(name);
+
+            await _productRepository.Save(product);
+        }
+    }
+}
